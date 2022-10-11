de044c286399462dec4e217c53d00385ea6cd682
Tony Rogvall <tony@rogvall.se>
2022-10-11 14:53:29 +0200

    allow multiple chan,note running on multiple voices

--- src/synthwave.erl
+++ src/synthwave.erl
@@ -8,8 +8,10 @@
 -module(synthwave).
 
 -export([start/0, start/1]).
--export([start_lpk25/0, start_vmpk/0, start_vpc1/0]).
+-export([start_lpk25/0, start_vmpk/0, start_usb_midi/0]).
 -export([start_device/1]).
+%% internal
+-export([iloop/1]).
 
 -include_lib("midi/include/midi.hrl").
 
@@ -30,21 +32,26 @@
 wdef(Note, Velocity, Amp) ->
     Freq1 = alsa_util:midi_note_to_frequency(Note),
     Freq2 = alsa_util:midi_note_to_frequency(max(127,Note+4)),
-    V = Velocity / 254,
-    [{adsr, 0.05, 0.05, 0.4, 0.1},
-     {wave, 0, [#{ form=>sine, freq=>Freq1, level=>0.0},
-		#{ form=>sine, freq=>Freq1+50, level=>(V+0.5)*Amp},
-		#{ form=>triangle, freq=>Freq1, level=>0.5*Amp},
-		#{ form=>sine, freq=>Freq1, level=>0.5*Amp},
-		#{ form=>sine, freq=>Freq1-10, level=>0.0}
-	       ]},
-     {wave, 1, [#{ form=>sine, freq=>Freq2, level=>0.1*Amp},
-		#{ form=>sine, freq=>Freq2, level=>0.1*Amp},
-		#{ form=>sine, freq=>Freq2, level=>0.1*Amp},
-		#{ form=>sine, freq=>Freq2, level=>0.1*Amp},
-		#{ form=>sine, freq=>Freq2, level=>0.1*Amp}
-	       ]}
-    ].
+    V = max(0.5, Velocity / 254),
+    D = max(0.3, Velocity / 127),
+    Ts = [0.04, 0.06, 0.1, 0.1+D],
+    High = 0.3,
+    Peek = (High+V),
+    {round(lists:sum(Ts)*1000),
+     [{envelope, Ts},
+      {wave, 0, [#{ form=>sine, freq=>Freq1, level=>0.0},
+		 #{ form=>sine, freq=>Freq1+100, level=>Peek*Amp},
+		 #{ form=>triangle, freq=>Freq1, level=>Peek*Amp},
+		 #{ form=>triangle, freq=>Freq1, level=>High*Amp},
+		 #{ form=>sine, freq=>Freq1, level=>0.0 }
+		]},
+      {wave, 1, [#{ form=>sine, freq=>Freq2, level=>0.2*Amp},
+		 #{ form=>sine, freq=>Freq2, level=>0.2*Amp},
+		 #{ form=>sine, freq=>Freq2, level=>0.2*Amp},
+		 #{ form=>sine, freq=>Freq2, level=>0.2*Amp},
+		 #{ form=>sine, freq=>Freq2, level=>0.2*Amp}
+		]}
+     ]}.
 
 
 start() -> start_lpk25().
@@ -55,8 +62,8 @@ start_lpk25() ->
 start_vmpk() ->
     start("VMPK").
 
-start_vpc1() ->
-    start("VPC1").
+start_usb_midi() ->
+    start("USB-MIDI").
 
 start(Name) ->
     Ds = midi:devices(),
@@ -71,7 +78,12 @@ start(Name) ->
 		    {error, not_found}
 	    end;
 	_ ->
-	    {error, not_found}
+	    case midi:shared_input(Name) of
+		#{ device := Device } ->
+		    start_device(Device);
+		_ ->
+		    {error, not_found}
+	    end
     end.
 
 
@@ -79,7 +91,7 @@ start_device(InputDevice) ->
     {ok,In}  = midi:open(InputDevice,[event,list,running]),
     Voices = lists:seq(1,?MAX_VOICES),
     alsa_play:start(#{ rate => 44100 }), %% latency => 50
-    Def = wdef(?C, 1, 0),
+    {_Dur,Def} = wdef(?C, 1, 0),
     lists:foreach(fun(I) ->
 			  alsa_play:new(I),
 			  alsa_play:stop(I),
@@ -104,8 +116,8 @@ sloop(State=#state{midi_in=In}) ->
     receive
 	{select,In,undefined,ready_input} ->
 	    oloop(State);
-	{_Ref,_Voice,_Pos, {off,Chan,Note}} ->
-	    State1 = off(Chan, Note, false, State),
+	{Ref,_Voice,_Pos, {off,Chan,Note}} ->
+	    State1 = off(Chan, Note, Ref, false, State),
 	    sloop(State1)
     end.
 
@@ -120,11 +132,11 @@ oloop(State=#state{midi_in=In}) ->
 	    io:format("midi event ~p\n", [Event]),
 	    State1 = midi_event(Event, State),
 	    oloop(State1);
-	{_Ref,_Voice,_Pos, {off,Chan,Note}} ->
-	    State1 = off(Chan, Note, false, State),
+	{Ref,_Voice,_Pos, {off,Chan,Note}} ->
+	    State1 = off(Chan, Note, Ref, false, State),
 	    oloop(State1)
     after 0 ->
-	    iloop(State)
+	    ?MODULE:iloop(State)
     end.    
 
 midi_event({note_on,Chan,Note,Velocity}, State) ->
@@ -142,33 +154,41 @@ note_on(Chan,Note,0,State) ->
 note_on(Chan,Note,Velocity,State) ->
     case allocate_voice(State#state.voices) of
 	{V,Vs} ->
-	    io:format("~w: allocate ~w, vs:~p\n", [{Chan,Note}, V, Vs]),
-	    alsa_play:set_wave(V, wdef(Note,Velocity,1)),
-	    alsa_play:mark(V, {time,600}, [stop,once,{set,bof},notify],
-			   {off,Chan,Note}),
+	    {Dur,WDef} = wdef(Note,Velocity,1),
+	    alsa_play:set_wave(V, WDef),
+	    {ok,Ref} = alsa_play:mark(V,{time,Dur},
+				      [stop,once,{set,bof},notify],
+				      {off,Chan,Note}),
+	    io:format("~w: allocate ~w=>~w, vs:~p\n",
+		      [{Chan,Note},Ref,V,Vs]),
 	    alsa_play:restart(V),
 	    alsa_play:run(V),
-	    Active = maps:put({Chan,Note}, V, State#state.active),
+	    Active0 = State#state.active,
+	    RefList = maps:get({Chan,Note}, Active0, []),
+	    Active = Active0# { Ref => V, {Chan,Note} => [Ref|RefList] },
 	    State#state { voices = Vs, active = Active };
 	false ->
 	    State
     end.
 
 note_off(_Chan,_Note,_Velocity,State) ->
-    %% off(Chan, Node, true, State),
+    %% off(Chan, Node, undefined, true, State),
     State.
 
-off(Chan,Note,Stop,State) ->
-    case maps:take({Chan,Note}, State#state.active) of
-     	{V, Active1} ->
+off(Chan,Note,Ref,Stop,State) ->
+    case maps:take(Ref, State#state.active) of
+	{V, Active1} ->
 	    if Stop ->
 		    alsa_play:stop(V);
 	       true ->
 		    ok
 	    end,
+	    RefList = lists:delete(Ref,maps:get({Chan,Note},Active1,[])),
+	    Active2 = Active1#{ {Chan,Note} => RefList },
 	    Voices = release_voice(V, State#state.voices),
-	    io:format("~w: release ~w, vs: ~p\n", [{Chan,Note},V,Voices]),
-	    State#state { voices = Voices, active = Active1 };
+	    io:format("~w: release ~w=>~w, vs: ~p\n",
+		      [{Chan,Note},Ref,V,Voices]),
+	    State#state { voices = Voices, active = Active2 };
 	error -> 
 	    State
     end.
